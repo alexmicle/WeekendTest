e2624a28db8ba5c05c6c8769823f940350a908b6
alexm <alexmicle@gmail.com>
2017-02-25 10:55:27 +0200

    --change print to new message

--- src/Example.java
+++ src/Example.java
@@ -6,7 +6,7 @@
 public class Example {
 
     public static void main(String[] args) {
-
+        System.out.println("Good bye!");
         int sum = Calc.add(2,3);
         System.out.println(sum); //5
 
