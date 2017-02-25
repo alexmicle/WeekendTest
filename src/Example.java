ed84674a2900468c13e21fa9959a84d38fa62c33
alexm <alexmicle@gmail.com>
2017-02-25 11:08:01 +0200

    --changes

--- src/Example.java
+++ src/Example.java
@@ -7,8 +7,8 @@ public class Example {
 
     public static void main(String[] args) {
         System.out.println("Good bye!");
-        int suma = Calc.add(2,3);
-        System.out.println(suma); //5
+        int suma = Calc.add(7,10);
+        System.out.println(suma);
 
     }
 
